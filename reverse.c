779735d8796adb3b6eb89cdca57ce4c2903975f2
suraj  kumar <singhsurajsi905@gmail.com>
2025-11-01 01:04:24 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n, a;
+    scanf("%d",&n);
+a=10;
+    for(int i=10;i<n;i--){
+        
+        printf("%d",i);
+        a=a-i;
+        
+    }
+    return 0;
+}
